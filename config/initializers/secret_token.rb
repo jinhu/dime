9eaf013b9c6744f41d83a37ce4a8f8c826ee4dfe
jin hu <jin@hukelou.com>
2011-04-14 00:54:14 +0200

    first version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dime2rescue::Application.config.secret_token = '08b00271db9da21faa37839cfc2954dfd9d8a0cf9fe54f25888399cfa12bbe6f286ae9c4d31f26fcfe7d0fa8532991b075a15c81068ab545d9dd0fb9bdb9aa69'
